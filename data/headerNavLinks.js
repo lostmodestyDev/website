74851cbd7a95109e2552874cfbdaa8de249016bc
LostModestyDev <lostmodesty@gmail.com>
2022-05-28 17:11:33 +0600

    Update fonts

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'ব্লগ' },
+  { href: '/tags', title: 'তালিকাসমূহ' },
+  { href: '/projects', title: 'আমাদের কাজ' },
+  { href: '/about', title: 'আমাদের সম্পর্কে' },
 ]
 
 export default headerNavLinks
