56105b2f1c32d532d4ace1a5a143cf1a164f57b4
Lostmodsty Dev <lostmodesty@gmail.com>
2022-06-30 14:45:52 +0600

    Fix post layout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -90,14 +90,11 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   </div>
                   {children}
                 </div>
-                {children}
               </div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(slug)} rel="nofollow">
                   {'Discuss on Twitter'}
                 </Link>
-                {` • `}
-                <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
               </div>
               <Comments frontMatter={frontMatter} />
             </div>
