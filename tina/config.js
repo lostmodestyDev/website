5082221a783de4b092e8120243ab37f2ffed4101
Lostmodsty Dev <lostmodesty@gmail.com>
2023-05-01 16:12:51 +0600

    Add tina cms

---
+++ tina/config.js
@@ -0,0 +1,62 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "6d0bca18-f530-49aa-911b-678a7790b17f", // Get this from tina.io
+  token: "9e9115d0fcc5e1b8f232022dc99587536e8b7a6f", // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Blog Posts",
+        name: "blog_posts",
+        path: "data/blog",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...blog_postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Authors",
+        name: "authors",
+        path: "data/authors",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
---
+++ tina/templates.js
@@ -0,0 +1,36 @@
+export function blog_postFields() {
+  return [
+    {
+      type: "string",
+      name: "title",
+      label: "title",
+    },
+    {
+      type: "string",
+      name: "description",
+      label: "description",
+    },
+    {
+      type: "datetime",
+      name: "date",
+      label: "date",
+    },
+    {
+      type: "string",
+      name: "redirect_from",
+      label: "redirect_from",
+      list: true,
+    },
+    {
+      type: "string",
+      name: "tags",
+      label: "tags",
+      list: true,
+    },
+    {
+      type: "image",
+      name: "hero",
+      label: "hero",
+    },
+  ];
+}
