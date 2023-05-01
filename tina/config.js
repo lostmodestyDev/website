852302580f5a803ab22a9e3bd21a2772a5da0c37
Lostmodsty Dev <lostmodesty@gmail.com>
2023-05-01 16:28:54 +0600

    Fix tina build

--- tina/config.js
+++ tina/config.js
@@ -1,62 +1,63 @@
-import { defineConfig } from "tinacms";
+import { defineConfig } from 'tinacms'
+import { blog_postFields } from './templates'
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
   branch,
-  clientId: "6d0bca18-f530-49aa-911b-678a7790b17f", // Get this from tina.io
-  token: "9e9115d0fcc5e1b8f232022dc99587536e8b7a6f", // Get this from tina.io
+  clientId: '6d0bca18-f530-49aa-911b-678a7790b17f', // Get this from tina.io
+  token: '9e9115d0fcc5e1b8f232022dc99587536e8b7a6f', // Get this from tina.io
   client: { skip: true },
   build: {
-    outputFolder: "admin",
-    publicFolder: "public",
+    outputFolder: 'admin',
+    publicFolder: 'public',
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+      mediaRoot: '',
+      publicFolder: 'public',
     },
   },
   schema: {
     collections: [
       {
-        format: "md",
-        label: "Blog Posts",
-        name: "blog_posts",
-        path: "data/blog",
+        format: 'md',
+        label: 'Blog Posts',
+        name: 'blog_posts',
+        path: 'data/blog',
         match: {
-          include: "**/*",
+          include: '**/*',
         },
         fields: [
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body of Document',
+            description: 'This is the markdown body',
             isBody: true,
           },
           ...blog_postFields(),
         ],
       },
       {
-        format: "md",
-        label: "Authors",
-        name: "authors",
-        path: "data/authors",
+        format: 'md',
+        label: 'Authors',
+        name: 'authors',
+        path: 'data/authors',
         match: {
-          include: "**/*",
+          include: '**/*',
         },
         fields: [
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body of Document',
+            description: 'This is the markdown body',
             isBody: true,
           },
         ],
       },
     ],
   },
-});
+})
--- tina/templates.js
+++ tina/templates.js
@@ -1,36 +1,36 @@
 export function blog_postFields() {
   return [
     {
-      type: "string",
-      name: "title",
-      label: "title",
+      type: 'string',
+      name: 'title',
+      label: 'title',
     },
     {
-      type: "string",
-      name: "description",
-      label: "description",
+      type: 'string',
+      name: 'description',
+      label: 'description',
     },
     {
-      type: "datetime",
-      name: "date",
-      label: "date",
+      type: 'datetime',
+      name: 'date',
+      label: 'date',
     },
     {
-      type: "string",
-      name: "redirect_from",
-      label: "redirect_from",
+      type: 'string',
+      name: 'redirect_from',
+      label: 'redirect_from',
       list: true,
     },
     {
-      type: "string",
-      name: "tags",
-      label: "tags",
+      type: 'string',
+      name: 'tags',
+      label: 'tags',
       list: true,
     },
     {
-      type: "image",
-      name: "hero",
-      label: "hero",
+      type: 'image',
+      name: 'hero',
+      label: 'hero',
     },
-  ];
+  ]
 }
